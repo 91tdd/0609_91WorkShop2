07f3c4ae5f93796b5e63c078feb08d7b639a3388
cloud.chen <thirmoon@gmail.com>
2018-06-09 16:18:07 +0800

    add first Test

--- WorkShop2/WorkShop22/Class1Tests.cs
+++ WorkShop2/WorkShop22/Class1Tests.cs
@@ -1,11 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using WorkShop2;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
 namespace WorkShop2.Tests
 {
     [TestClass()]
@@ -14,9 +11,18 @@ namespace WorkShop2.Tests
         [TestMethod()]
         public void ResultTest()
         {
-            var class1 = new Class1();
-            class1.Result();
-            Assert.Fail();
+            IRepository<Budget> repo = Substitute.For<IRepository<Budget>>();
+            repo.GetBudgets().Returns(new List<Budget>()
+            {
+                new Budget() { YearMonth = "201802", Amount = 280 },
+                new Budget() { YearMonth = "201806", Amount = 300 },
+                new Budget() { YearMonth = "201807", Amount = 310 }
+
+            });
+            var target = new WorkShop22.BudgetCalculate(repo);
+
+            var actual = target.Result(new DateTime(2018,6,1),new DateTime(2018,6,30));
+            Assert.AreEqual(300m, actual);
         }
     }
 }
