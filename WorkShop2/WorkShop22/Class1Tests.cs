72e3e5e14b2c4b47739293852cd20086f8e5b4d1
hatelove <jay.hatelove@gmail.com>
2018-06-09 19:45:13 +0800

    1. rename: BudgetCalculator class, _budgetRepository field, GivenBudgets()
    2. change GivenBudgets() signature, and initialize budgetCalculator in [TestInitialize]

--- WorkShop2/WorkShop22/Class1Tests.cs
+++ WorkShop2/WorkShop22/Class1Tests.cs
@@ -2,90 +2,98 @@
 using NSubstitute;
 using System;
 using System.Collections.Generic;
+using WorkShop22;
 
 namespace WorkShop2.Tests
 {
-    [TestClass()]
+    [TestClass]
     public class Class1Tests
     {
-        private IRepository<Budget> _budRepository = Substitute.For<IRepository<Budget>>();
-        private WorkShop22.BudgetCalculate _budgetCalculate;
+        private IRepository<Budget> _budgetRepository = Substitute.For<IRepository<Budget>>();
+        private BudgetCalculator _budgetCalculator;
 
         [TestInitialize]
         public void TestInit()
         {
-            _budgetCalculate = GiveBudgets();
+            _budgetCalculator = new BudgetCalculator(_budgetRepository);
+            GivenBudgets();
         }
 
-        [TestMethod()]
+        [TestMethod]
         public void OneMonthFullBudget()
         {
             BudgetResultShouldBe(new DateTime(2018, 6, 1), new DateTime(2018, 6, 30), 300m);
         }
 
-        [TestMethod()]
+        [TestMethod]
         public void OneMonthPartialBudget()
         {
             BudgetResultShouldBe(new DateTime(2018, 6, 1), new DateTime(2018, 6, 15), 150m);
         }
-        [TestMethod()]
+
+        [TestMethod]
         public void TwoMonthBothNotBudget()
         {
             BudgetResultShouldBe(new DateTime(2018, 3, 1), new DateTime(2018, 4, 2), 0m);
         }
-        [TestMethod()]
+
+        [TestMethod]
         public void TwoMonth_StartMonthNoBudget_EndMonthHasBudget()
         {
             BudgetResultShouldBe(new DateTime(2018, 5, 20), new DateTime(2018, 6, 10), 100m);
         }
-        [TestMethod()]
+
+        [TestMethod]
         public void TwoMonth_StartMonthHasBudget_EndMonthNoBudget()
         {
             BudgetResultShouldBe(new DateTime(2018, 7, 30), new DateTime(2018, 8, 22), 20m);
         }
-        [TestMethod()]
+
+        [TestMethod]
         public void TwoMonthBothBudget()
         {
             BudgetResultShouldBe(new DateTime(2018, 6, 15), new DateTime(2018, 7, 14), 300m);
         }
-        [TestMethod()]
+
+        [TestMethod]
         public void OverYearBudget()
         {
             BudgetResultShouldBe(new DateTime(2017, 12, 1), new DateTime(2018, 2, 1), 10m);
         }
-        [TestMethod()]
+
+        [TestMethod]
         public void OverYearBudget_EndYearHas2Budget()
         {
             BudgetResultShouldBe(new DateTime(2017, 11, 1), new DateTime(2018, 6, 30), 580m);
         }
-        [TestMethod()]
+
+        [TestMethod]
         public void AllYearBudget()
         {
             BudgetResultShouldBe(new DateTime(2018, 01, 1), new DateTime(2018, 12, 31), 890);
         }
 
         [ExpectedException(typeof(ArgumentException))]
-        [TestMethod()]
+        [TestMethod]
         public void ThrowExpection()
         {
-            var result = _budgetCalculate.Result(new DateTime(2018, 5, 1), new DateTime(2018, 4, 30));
+            var result = _budgetCalculator.Result(new DateTime(2018, 5, 1), new DateTime(2018, 4, 30));
         }
+
         private void BudgetResultShouldBe(DateTime startTime, DateTime endTime, decimal expected)
         {
-            var actual = _budgetCalculate.Result(startTime, endTime);
+            var actual = _budgetCalculator.Result(startTime, endTime);
             Assert.AreEqual(expected, actual);
         }
 
-        private WorkShop22.BudgetCalculate GiveBudgets()
+        private void GivenBudgets()
         {
-            _budRepository.GetBudgets().Returns(new List<Budget>()
+            _budgetRepository.GetBudgets().Returns(new List<Budget>
             {
-                new Budget() {YearMonth = "201802", Amount = 280},
-                new Budget() {YearMonth = "201806", Amount = 300},
-                new Budget() {YearMonth = "201807", Amount = 310}
+                new Budget {YearMonth = "201802", Amount = 280},
+                new Budget {YearMonth = "201806", Amount = 300},
+                new Budget {YearMonth = "201807", Amount = 310}
             });
-            var target = new WorkShop22.BudgetCalculate(_budRepository);
-            return target;
         }
     }
 }
