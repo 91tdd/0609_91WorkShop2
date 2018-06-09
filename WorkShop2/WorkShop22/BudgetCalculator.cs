be06bd5a8433c6d1466c438e0f045e06a6b4a2aa
hatelove <jay.hatelove@gmail.com>
2018-06-09 23:51:53 +0800

    refactor BudgetCalculator class completed

--- WorkShop2/WorkShop22/BudgetCalculator.cs
+++ WorkShop2/WorkShop22/BudgetCalculator.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using WorkShop2.Tests;
 
@@ -17,26 +16,9 @@ namespace WorkShop22
         internal decimal TotalAmount(DateTime startTime, DateTime endTime)
         {
             var period = new Period(startTime, endTime);
-            var budgets = _budRepository.GetBudgets();
-            var budget = budgets.SingleOrDefault(x => x.YearMonth == period.StartTime.ToString("yyyyMM"));
 
-            if (period.IsSameMonth())
-            {
-                if (budget == null)
-                {
-                    return 0;
-                }
-
-                return period.Days() * budget.DailyAmount();
-            }
-
-            var total = TotalAmountWhenPeriodOverlapMultiMonths(period, budgets);
-            return total;
-        }
-
-        private static decimal TotalAmountWhenPeriodOverlapMultiMonths(Period period, List<Budget> budgets)
-        {
-            return budgets.Sum(b => b.OverlappingAmount(period));
+            return _budRepository.GetBudgets()
+                .Sum(b => b.OverlappingAmount(period));
         }
     }
 }
